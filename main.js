8540798f7d5fc36a982254c097a8ab8ebd3c2990
jtmp2r <jtmp2r@gmail.com>
2015-11-09 19:37:25 -0600

    added input field and two way data binding

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var app = angular.module("TodoApp", [])
 
 app.controller("TodoCtrl", function($scope) {
-  $scope.title = "Welcome to you ToDo App";
-});
+  $scope.title = "Welcome to your ToDo App";
+})
+
+app.controller("PastaCtrl", function($scope) {
+	$scope.pasta = "I love carbs";
+	$scope.foods = "";
+})
